d8e539077f40922545f46d28516b92005fab2d84
ppaulo-d <ppaulo-d@student.42sp.org.br>
2022-08-17 16:40:13 +0200

    Fixed FD reutilization and SEGFAULT when read is interrupted

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: ppaulo-d < ppaulo-d@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/26 00:53:11 by ppaulo-d          #+#    #+#             */
-/*   Updated: 2022/05/03 21:09:25 by ppaulo-d         ###   ########.fr       */
+/*   Updated: 2022/08/17 16:39:18 by ppaulo-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,11 +106,14 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
 	fill_buffer(fd, &s_buffer);
+	if (!s_buffer)
+		return (NULL);
 	s_buffer = fill_line(s_buffer, &line);
 	if (ft_strlen(line) == 0)
 	{
 		free(line);
 		free(s_buffer);
+		s_buffer = NULL;
 		return (NULL);
 	}
 	return (line);
